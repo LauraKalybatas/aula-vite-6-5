208709ab352342885cb09d4cd7c51d2e276e1335
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-17 12:56:56 -0300

    detalhes

--- src/pages/Filmes.jsx
+++ src/pages/Filmes.jsx
@@ -1,33 +1,41 @@
-import{ useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom';
 
 function Filmes() {
-  const [filmes, setFilmes] = useState([])
+ 
+    const [filmes, setFilmes] = useState([])
 
-  useEffect( () =>{
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e')
+    const apiKey= 'api_key=7c572a9f5b3ba776080330d23bb76e1e'
+    const urlBase = 'https://api.themoviedb.org/3/movie/'
+    const urlImg = 'https://image.tmdb.org/t/p/w342/'
+
+    useEffect(() => {
+
+    fetch(`${urlBase}popular?${apiKey}`)
     .then(response => response.json())
     .then(response => setFilmes(response.results))
-    .catch (error => console.log(error))
-  },[])
+    .catch(error => console.log(error))
 
-  return ( 
-    <>
-      <h1>Filmes</h1>
-      <div className="listaFilmes flex flex-rol gap-3">
+    },[])
+
+    return ( 
+        <>
+        <h1>Filmes</h1>
+        <div className="listaFilmes flex flex-row gap-3">
         {
-          filmes.map(
-            filme => (
-              <div className="card-filme" key={filme.id}>
-                <img src={`https://image.tmdb.org/t/p/w342/${filme.backdrop_path}`}/>
-                <h1>{filme.title}</h1>
-                <Link to={`filmes/:${filme.id}`}>Saber Mais</Link>
-              </div>
+                filmes.map(filme => (
+                    <div className="card-filme" key={filme.id}>
+                    <img className="w-auto" src={`${urlImg}${filme.poster_path}`}/>
+                    <h1>{filme.title}</h1>
+                    <Link to={`${filme.id}`} className="bg-blue-500">Saber Mais</Link>
+                    </div>
+                )
             )
-          )
-        } 
-      </div>
-    </>
-  );
+        }
+        </div>
+        </> 
+    )
 }
-export default Filmes
+
+
+export default Filmes ;
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,7 +1,7 @@
 function Sobre() {
     return(
       <>
-        <h1>Sobre</h1>
+        
       </>
     )
 }
