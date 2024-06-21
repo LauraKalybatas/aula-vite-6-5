30ba313dacc25c10549bb259d5be8b21c6dc17c3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-21 11:01:13 -0300

    home+

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,36 +27,66 @@ function Home() {
               <p className='text-amber-400 text-4xl'>Bem-vindo(a).</p>
               <p className='text-2xl'>Milhões de filmes, séries e artigos para descobrir. Explore já.</p>
               
-              <form className="max-w-md mx-auto mt-5">   
-                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+              <form className="max-w-md mx-auto mt-8">   
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
 
                 <div className="relative">
-                  <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                      <path className="stroke-currentColor stroke-linecap-round stroke-linejoin-round stroke-width-2 d-m19-19-4-4m0-7A7-7-0-1-1-1-8a7-7-0-0-1-14-0Z"/>
-                    </svg>
-                  </div>
 
-                  <input type="search" id="default-search" className="block w-full p-2 ps-10 text-sm rounded-full bg-gray-50 dark:bg-gray-700 dark:placeholder-gray-400 text-black" placeholder="Pesquise por título, gênero, elenco, etc..."/>
+                  <input type="search" id="default-search" className="block w-full p-2 text-sm rounded-full bg-transparent text-white" placeholder="Pesquise por título, gênero, elenco, etc..."/>
                 </div>
               </form>
             </div>
           </div>
 
-          <div className="listaFilmes flex flex-row overflow-x-scroll pt-7 hide-scroll-bar relative ">
+          <p className='text-white text-3xl font-bold ml-4 mt-4'>Filmes</p>
+          <div className="listaFilmes flex flex-row overflow-x-scroll pt-4 hide-scroll-bar relative pb-6">
                {
                 filmes.map(filme => (
                   <>
                     <div className="inline-block px-3 ">
                     <img className='absolute w-1/6 h-64 pl-5  pt-5' src={`${urlImg}${filme.poster_path}`} alt="" />
-                    <div className="w-56 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-stone-900 hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                      <h1 className="relative font-bold text-center text-white pt-64 ">{filme.title}</h1>
+                    <div className="w-56 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-zinc-800 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                      <h1 className="relative font-bold text-center text-white pt-64 ml-3 mr-3">{filme.title}</h1>
                     </div>
                     </div>
                 </>
                 ))}
           </div>
+
+          <p className='text-white text-3xl font-bold ml-4 mt-4'>Artigos</p>
+          <div className="listaFilmes flex flex-row overflow-x-scroll pt-4 hide-scroll-bar relative pb-6">
+               {
+                filmes.map(filme => (
+                  <>
+                    <div className="inline-block px-3 ">
+                    <img className='absolute w-1/6 h-64 pl-5  pt-5' src={`${urlImg}${filme.poster_path}`} alt="" />
+                    <div className="w-56 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-zinc-800 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                      <h1 className="relative font-bold text-center text-white pt-64 ml-3 mr-3">{filme.title}</h1>
+                    </div>
+                    </div>
+                </>
+                ))}
+          </div>
+
+          <p className='text-white text-3xl font-bold ml-4 mt-4'>Recomendações</p>
+          <div className="listaFilmes flex flex-row overflow-x-scroll pt-4 hide-scroll-bar relative pb-6">
+               {
+                filmes.map(filme => (
+                  <>
+                    <div className="inline-block px-3 ">
+                    <img className='absolute w-1/6 h-64 pl-5  pt-5' src={`${urlImg}${filme.poster_path}`} alt="" />
+                    <div className="w-56 h-80 max-w-xs overflow-hidden rounded-lg shadow-md bg-zinc-800 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                      <h1 className="relative font-bold text-center text-white pt-64 ml-3 mr-3">{filme.title}</h1>
+                    </div>
+                    </div>
+                </>
+                ))}
+          </div>
+
       </main>
+
+      <footer>
+      </footer>
     </>
   )
 }
